a3a58ec5e886667340226c17a2a7705c3c05bb83
Zonova <saad1225@gmail.com>
2023-05-09 11:40:30 -0400

    adding model training

--- radar_nowcasting/RadarNowcast_torch.py
+++ radar_nowcasting/RadarNowcast_torch.py
@@ -93,4 +93,90 @@ Y_val = Y_val.transpose(0,3,1,2)
 X_test = X_test.transpose(0,3,1,2)
 Y_test = Y_test.transpose(0,3,1,2)
 
+# Add more as needed
+params={
+    'start_neurons'   :16,      # Controls size of hidden layers in CNN, higher = more complexity 
+    #'activation'      :'relu',  # Activation used throughout the U-Net,  see https://www.tensorflow.org/api_docs/python/tf/keras/activations
+    'activation'      :nn.ReLU,  # Activation used throughout the U-Net,  see https://www.tensorflow.org/api_docs/python/tf/keras/activations
+    'loss'            :'mae',   # Either 'mae' or 'mse', or others as https://www.tensorflow.org/api_docs/python/tf/keras/losses
+    'loss_weights'    :0.021,    # Scale for loss.  Recommend squaring this if using MSE
+    #'opt'             :tf.keras.optimizers.Adam,  # optimizer, see https://www.tensorflow.org/api_docs/python/tf/keras/optimizers
+    'opt'             :torch.optim.Adam,  # optimizer, see https://www.tensorflow.org/api_docs/python/tf/keras/optimizers
+    'learning_rate'   :0.001,   # Learning rate for optimizer
+    'num_epochs'      :10,       # Number of epochs to train for
+    'batch_size'      :8        # Size of batches during training
+}
+
+#unet = create_model(start_neurons=params['start_neurons'],activation=params['activation']) 
+unet = UNet(start_neurons=params['start_neurons'],activation=params['activation'])
+
+summary(unet,input_size = (8,13,384, 384))
+
+optimizer = params['opt'](unet.parameters(), lr=params['learning_rate'])
+if params['loss'] == 'mae':
+    loss_function = nn.L1Loss()
+elif params['loss'] == 'mse':
+    loss_function = nn.MSELoss()
+else:
+    raise ValueError(f"Unsupported loss function: {params['loss']}")
+
+import torch.utils.data as data
+from torch.utils.tensorboard import SummaryWriter
+
+num_epochs = params['num_epochs']
+batch_size = params['batch_size']
+
+train_losses = []
+mean_val_losses = []
+
+# Create a TensorBoard logger
+exprmt_dir = make_log_dir('experiments')
+writer = SummaryWriter(log_dir=exprmt_dir + '/tboardlogs')
+
+# Create PyTorch Datasets and DataLoaders for training and validation
+train_dataset = data.TensorDataset(torch.from_numpy(X_train), torch.from_numpy(Y_train))
+val_dataset = data.TensorDataset(torch.from_numpy(X_val), torch.from_numpy(Y_val))
+
+train_loader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+# Define the device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Move the model to the device
+unet = unet.to(device)
+
+# Training loop
+for epoch in range(num_epochs):
+    unet.train()
+    for i, (x_batch, y_batch) in enumerate(train_loader):
+        optimizer.zero_grad()
+        
+        x_batch, y_batch = x_batch.to(device), y_batch.to(device)
+        outputs = unet(x_batch)
+        loss = loss_function(outputs, y_batch)
+        
+        loss.backward()
+        optimizer.step()
+    train_losses.append(0.021*loss.item()) #saving the training_loss for this epoch, multiplying by the "loss_weights" = 0.021 to match tensorflow code.
+    
+    unet.eval()
+    val_losses = []
+    with torch.no_grad():
+        for x_val_batch, y_val_batch in val_loader:
+            x_val_batch, y_val_batch = x_val_batch.to(device), y_val_batch.to(device)
+            val_outputs = unet(x_val_batch)
+            val_loss = loss_function(val_outputs, y_val_batch)
+            val_losses.append(val_loss.item())
+
+    mean_val_loss = np.mean(val_losses)
+    mean_val_losses.append(0.021*mean_val_loss)#multiplying by the "loss_weights" = 0.021 to match tensorflow code.
+    print(f"Epoch {epoch + 1}/{num_epochs}, Validation Loss: {0.021*mean_val_loss}")
+
+    # Save the model with the best validation loss
+    torch.save(unet.state_dict(), f"{exprmt_dir}/nowcast-unet-{epoch + 1:04d}-{mean_val_loss:.4f}.pt")
+
+    # Log the validation loss to TensorBoard
+    writer.add_scalar("Validation Loss", 0.021*mean_val_loss, epoch)
+writer.close()
 
